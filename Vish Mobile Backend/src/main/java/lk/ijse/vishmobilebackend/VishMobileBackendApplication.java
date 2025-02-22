0749189f1b1371a9761e76eaece6d5e9eff9ca1c
hirusha <hirushaperera651@gmail.com>
2025-02-22 10:49:38 +0530

    Initial commit

---
+++ Vish Mobile Backend/src/main/java/lk/ijse/vishmobilebackend/VishMobileBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.vishmobilebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishMobileBackendApplication {
+
+    public static void main(String[] args)   {
+        SpringApplication.run(VishMobileBackendApplication.class, args);
+    }
+
+}
---
+++ Vish Mobile Backend/src/test/java/lk/ijse/vishmobilebackend/VishMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.vishmobilebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VishMobileBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
